4f2bae2114da7e72bd0c789bc3815821dfcfca94
YoungWan Kwon <87213416+000wan@users.noreply.github.com>
2024-04-21 00:59:48 +0900

    Test fail

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <array>
-#include <cmath>
-#include <cassert>
-#include <limits>
+// #include <cmath>
+// #include <cassert>
 
 using namespace std;
 
@@ -9,31 +8,36 @@ using namespace std;
 /// Real
 
 // Define the number of fractional bits
-constexpr int FRACTION_BITS = 10; // max 13
+const int FRACTION_BITS = 10; // max 13
 
 // Define the maximum and minimum values for the fixed-point number
-constexpr int MAX_VALUE = (1 << (std::numeric_limits<int>::digits - FRACTION_BITS - 1)) - 1;
-constexpr int MIN_VALUE = -(1 << (std::numeric_limits<int>::digits - FRACTION_BITS - 1));
+const int MAX_VALUE = (1 << (32 - FRACTION_BITS - 1)) - 1;
+const int MIN_VALUE = -(1 << (32 - FRACTION_BITS - 1));
+
+
 
 class FixedPoint {
 private:
-    int value;
-    FixedPoint(int value = 0) : value(value) {}
-
 public:
-    FixedPoint(float f) : value(static_cast<int>(f * (1 << FRACTION_BITS))) {}
-    FixedPoint(double f) : value(static_cast<int>(f * (1 << FRACTION_BITS))) {}
-
+    int value;
+    
+    FixedPoint(int value = 0, bool value_alloc = 0) : value((value_alloc ? value : value * (1 << FRACTION_BITS))) {}
     static FixedPoint from(int x) {
-        return FixedPoint(x * (1 << FRACTION_BITS));
-    }
-    static FixedPoint from(float x) {
         return FixedPoint(x);
     }
 
+    // float not supported in zk llvm
+    // FixedPoint(float f) : value(static_cast<int>(f * (1 << FRACTION_BITS))) {}
+    // FixedPoint(double f) : value(static_cast<int>(f * (1 << FRACTION_BITS))) {}
+    // static FixedPoint from(float x) {
+    //     return FixedPoint(x);
+    // }
+    // operator float() const {
+    //     return static_cast<float>(value) / (1 << FRACTION_BITS);
+    // }
 
-    operator float() const {
-        return static_cast<float>(value) / (1 << FRACTION_BITS);
+    operator int() const {
+        return value / (1 << FRACTION_BITS);
     }
 
     FixedPoint operator+(const FixedPoint& other) const {
@@ -43,7 +47,7 @@ public:
         } else if (sum < MIN_VALUE) {
             sum = MIN_VALUE;
         }
-        return FixedPoint(sum);
+        return FixedPoint(sum, 1);
     }
 
     FixedPoint operator-(const FixedPoint& other) const {
@@ -53,7 +57,7 @@ public:
         } else if (diff < MIN_VALUE) {
             diff = MIN_VALUE;
         }
-        return FixedPoint(diff);
+        return FixedPoint(diff, 1);
     }
 
     FixedPoint operator*(const FixedPoint& other) const {
@@ -64,21 +68,25 @@ public:
         } else if (product < MIN_VALUE) {
             product = MIN_VALUE;
         }
-        return FixedPoint(static_cast<int>(product));
+        return FixedPoint(static_cast<int>(product), 1);
     }
 
     FixedPoint operator/(const FixedPoint& other) const {
         if (other.value == 0) {
-            return FixedPoint(MAX_VALUE);
+            return FixedPoint(MAX_VALUE, 1);
         }
-        long long dividend = static_cast<long long>(value) << FRACTION_BITS;
-        long long quotient = dividend / other.value;
+        int dividend = static_cast<int>(value) << FRACTION_BITS;
+        if (dividend < value) {
+            throw "Divide Overflow";
+        }
+
+        int quotient = dividend / other.value;
         if (quotient > MAX_VALUE) {
             quotient = MAX_VALUE;
         } else if (quotient < MIN_VALUE) {
             quotient = MIN_VALUE;
         }
-        return FixedPoint(static_cast<int>(quotient));
+        return FixedPoint(static_cast<int>(quotient), 1);
     }
 
     FixedPoint powi(int exp) {
@@ -90,15 +98,24 @@ public:
     }
 };
 
-FixedPoint factorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result = result * i;
-    }
-    return FixedPoint::from(result);
+FixedPoint from2fp(int x) {
+    return FixedPoint(x * (1 << FRACTION_BITS));
 }
 
-typedef FixedPoint R;
+int fp2int(FixedPoint x) {
+    return x.value / (1 << FRACTION_BITS);
+}
+
+// FixedPoint factorial(int n) {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) {
+//         result = result * i;
+//     }
+//     return FixedPoint::from(result);
+// }
+
+// typedef FixedPoint R;
+#define R FixedPoint
 
 // Complex
 const int PRECISION = 10;
@@ -110,13 +127,7 @@ public:
 
     cpx () : real(0.0), imag(0.0) {}
     cpx (FixedPoint real, FixedPoint imag) : real(real), imag(imag) {}
-    static cpx from(int x) {
-        return cpx(R::from(x), 0.0);       
-    }
-    static cpx from(R x) {
-        return cpx(x, 0.0);
-    }
-
+    /*
     // arithmetic operations
     cpx operator+(const cpx& other) const {
         return cpx(real + other.real, imag + other.imag);
@@ -129,24 +140,33 @@ public:
     }
     cpx operator/(const cpx& other) const {
         FixedPoint denominator = other.real * other.real + other.imag * other.imag;
-        if (denominator == 0) {
-            return cpx(FixedPoint::from(MAX_VALUE), FixedPoint::from(MAX_VALUE));
+        if (fp2int(denominator) == 0) {
+            throw "Division by zero";
+            return cpx(MAX_VALUE, MAX_VALUE);
         }
-        cpx numerator = *this * cpx(other.real, -other.imag);
+        cpx numerator = *this * cpx(other.real, R(0)-other.imag);
         return cpx(numerator.real / denominator, numerator.imag / denominator);
     }
 
     cpx powi(int exp) {
-        cpx result = cpx::from(1);
+        cpx result = cpx(1, 0);
         for (int i = 0; i < exp; i++) {
             result = result * *this;
         }
         return result;
-    }
+    }*/
 };
-typedef cpx C;
+#define C cpx
+
+cpx from2cpx(int x) {
+    return cpx(from2fp(x), 0.0);       
+}
+cpx from2cpx(R x) {
+    return cpx(x, 0.0);
+}
 
 
+/*
 R sin_taylor(R x) {
     R result (0.0);
     int sign = 1;
@@ -169,18 +189,20 @@ R cos_taylor(R x) {
     return result;
 }
 
-#define M_PI 3.14159265358979323846
+// #define M_PI 3.14159265358979323846
 C find_mth_root_of_unity(int m) {
-    R ang = R::from((float)M_PI * 2 / m);
+    // R ang = R::from((float)M_PI * 2 / m);
+    R ang = R::from(4 * 2 / m);
     C zeta (cos_taylor(ang), sin_taylor(ang));
     return zeta;
 }
-
+*/
 
 bool is_power_of_two(int n) {
     return (n & (n - 1)) == 0;
 }
 
+/*
 int number_of_bits(int n) {
     int count = 0;
     while (n > 0) {
@@ -202,7 +224,7 @@ int bit_reverse_value(int x, int n) {
 
 template<size_t N>
 array<C, N> get_bit_reverse_array(const array<C, N>& a, int n) {
-    assert(is_power_of_two(n));
+    __builtin_assigner_exit_check(is_power_of_two(n));
 
     array<C, N> r = {};
     for (int i = 0; i < n; i++) {
@@ -242,17 +264,9 @@ array<int, N> get_rot_group(int N_half, int M) {
 }
 
 
-const int N = 4;
-[[circuit]] array<C, N> specialFFT(array<int, N> a_input, int n, int M) {
-    assert(a_input.size() == n);
-    assert(n == N);
-    assert(is_power_of_two(n));
-
-    array<C, N> a;
-    for (int i = 0; i < n; i++) {
-        a[i] = C::from(a_input[i]);
-    }
-
+template <size_t N>
+// [[circuit]] array<C, N> specialFFT([[private_input]] array<int, N> a_input, int n, int M) {
+array<C, N> specialFFT(array<C, N> a, int n, int M) {
     array<C, N> b = get_bit_reverse_array(a, n);
     array<C, N> psi_powers = get_psi_powers<N>(M);
     array<int, N> rot_group = get_rot_group<N>(M >> 2, M);
@@ -280,8 +294,8 @@ const int N = 4;
 
 template<size_t N>
 array<C, N> specialIFFT(const array<C, N>& a, int n, int M) {
-    assert(a.size() == n);
-    assert(is_power_of_two(n));
+    __builtin_assigner_exit_check(a.size() == n);
+    __builtin_assigner_exit_check(is_power_of_two(n));
 
     array<C, N> b = a;
 
@@ -314,4 +328,41 @@ array<C, N> specialIFFT(const array<C, N>& a, int n, int M) {
     }
 
     return b;
+}
+*/
+
+/// Circuit
+#include <iostream>
+const int N = 4;
+
+array<int, 2*N> complex2int(array<C, N>& a, int n) {
+    array<int, 2*N> r = {};
+    for (int i = 0; i < N; i++) {
+        r[2*i] = fp2int(a[i].real);
+        r[2*i+1] = fp2int(a[i].imag);
+        // std::cout << r[2*i] << " " << r[2*i+1] << std::endl;
+    }
+
+    return r;
+}
+
+[[circuit]] array<int, 2*N> main_circuit(int n, int M) {
+// [[circuit]] array<int, 2*N> main_circuit([[private_input]] array<int, N> a_input, int n, int M) {
+    array<int, N> a_input = {1, 2, 3, 4};
+
+    // __builtin_assigner_exit_check(a_input.size() == n);
+    // __builtin_assigner_exit_check(n == N);
+    // __builtin_assigner_exit_check(is_power_of_two(n));
+
+    array<cpx, N> a = {};
+    for (int i = 0; i < N; i++) {
+        a[i] = from2cpx(a_input[i]);
+        // std::cout << a[i].real.value << " " << a[i].imag.value << std::endl;
+    }
+
+    array<int, 2*N> output = complex2int(a, n);
+    // array<C, N> res = specialFFT<N>(a, n, M);
+    // array<int, 2*N> output = complex2int<N>(res);
+
+    return output;
 }
