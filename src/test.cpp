75c4d685b1f15fe85e061f9606c333a1bfc1ceda
indextree <brilliantepoch22@gmail.com>
2024-04-21 00:57:16 +0900

    added unit tests

--- src/test.cpp
+++ src/test.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+const float eps = 1e-2;
+
 
 /// Real
 
@@ -31,7 +33,6 @@ public:
     static FixedPoint from(float x) {
         return FixedPoint(x);
     }
-
 
     operator float() const {
         return static_cast<float>(value) / (1 << FRACTION_BITS);
@@ -83,26 +84,31 @@ public:
     }
 
     FixedPoint powi(int exp) {
-        FixedPoint result = FixedPoint(1);
+        FixedPoint result = FixedPoint(1.0);
         for (int i = 0; i < exp; i++) {
             result = result * *this;
         }
         return result;
     }
-};
 
-FixedPoint factorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result = result * i;
+    static FixedPoint factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return FixedPoint::from(result);
     }
-    return FixedPoint::from(result);
-}
+
+    FixedPoint abs() {
+        if (*this < 0) return -*this;
+        else return *this;
+    }
+};
 
 typedef FixedPoint R;
 
 // Complex
-const int PRECISION = 10;
+const int PRECISION = 4;
 
 class cpx {
 public:
@@ -152,7 +158,7 @@ R sin_taylor(R x) {
     R result (0.0);
     int sign = 1;
     for (int n=0; n<PRECISION; n++) {
-        R term = R::from(sign) * x.powi(2*n+1) / factorial(2*n+1);
+        R term = R::from(sign) * x.powi(2*n+1) / R::factorial(2*n+1);
         result = result + term;
         sign *= -1;
     }
@@ -163,7 +169,7 @@ R cos_taylor(R x) {
     R result (0.0);
     int sign = 1;
     for (int n=0; n<PRECISION; n++) {
-        R term = R::from(sign) * x.powi(2*n) / factorial(2*n);
+        R term = R::from(sign) * x.powi(2*n) / R::factorial(2*n);
         result = result + term;
         sign *= -1;
     }
@@ -177,22 +183,87 @@ C find_mth_root_of_unity(int m) {
     return zeta;
 }
 
-
+void FpArithmeticTest();
+void FpFunctionTest();
+void CpxArithmeticTest();
+void CpxFunctionTest();
+void sinTest();
+void cosTest();
+void rootTest();
 
 int main() {
-    FixedPoint a (1.1234f);
-    FixedPoint b (1.0f);
-    FixedPoint c = a + b;
-    FixedPoint d = a - b;
-    FixedPoint e = a * b;
-    FixedPoint f = a / b;
-
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-    std::cout << "a + b = " << c << std::endl;
-    std::cout << "a - b = " << d << std::endl;
-    std::cout << "a * b = " << e << std::endl;
-    std::cout << "a / b = " << f << std::endl;
-
+    FpArithmeticTest();
+    FpFunctionTest();
+    CpxArithmeticTest();
+    CpxFunctionTest();
+    sinTest();
+    cosTest();
+    rootTest();
     return 0;
+}
+
+void FpArithmeticTest() {
+    cout << "FpArithmeticTest start" << "\n";
+    R f1 = 1.23, f2 = 4.56, f3 = 7.89;
+    R res = f1 + f2 + f3;
+    R diff = res - R(13.68);
+
+    if (diff < R(0.0)) diff = -diff;
+    assert(diff < 1);
+
+    f1 = 12.34, f2 = 56.78;
+    res = f1 * f2;
+    diff = res - R(700.6652);
+    if (diff < R(0.0)) diff = -diff;
+    assert(diff < 1);
+    cout << "FpArithmeticTest end" << "\n";
+}
+
+void FpFunctionTest() { // powi, factorial, abs
+    cout << "FpFunctionTest start" << "\n";
+    R f1 = 1.23, f2 = 4.56, f3 = -1.23;
+
+    assert(abs(f1) == abs(f3));
+
+    R res = f1.powi(5);
+    R diff = abs(res - R(2.28886641));
+    assert(diff < 1);
+
+    res = R::factorial(7);
+    assert(R(5040.0) == res);
+
+    cout << "FpFunctionTest end" << "\n";
+}
+
+void CpxArithmeticTest() {
+    cout << "CpxArithmeticTest start" << "\n";
+    cpx A (1.23, 4.56), B (1.23, -2.34);
+    cpx C = A + B;
+    cpx diff = C - cpx(2.46, 2.22);
+    assert(diff.real.abs() < 1 && diff.imag.abs() < 1);
+
+    C = A * B;
+    diff = C - cpx(12.1833, 2.7306);
+    assert(diff.real.abs() < 1 && diff.imag.abs() < 1);
+    cout << "CpxArithmeticTest end" << "\n";
+}
+
+void CpxFunctionTest() {
+    cout << "CpxFunctionTest start" << "\n";
+    cpx A (1.23, 4.56);
+    cpx diff = A.powi(3) - cpx(-74.8675, -74.1223);
+    assert(diff.real.abs() < 1 && diff.imag.abs() < 1);
+    cout << "CpxFunctionTest end" << "\n";
+}
+
+void sinTest() {
+    
+}
+
+void cosTest() {
+
+}
+
+void rootTest() {
+
 }
